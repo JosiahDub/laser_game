bce5a70f82bb40e2e2f523a5359baa7969d6d173
Josiah White <josiahwhite@gmail.com>
2019-01-19 13:04:43 -0500

    Moving turrets to center.

--- play_missile_defense.py
+++ play_missile_defense.py
@@ -1,10 +1,10 @@
 import Adafruit_PCA9685
 from games import MissileDefense
 from src import pro_controller_factory
-from src.TURRETS import TURRET_1, TURRET_2, TURRET_3
+from src.TURRETS import TURRET_2, TURRET_3, TURRET_4
 
 left_pro, _ = pro_controller_factory()
 pwm = Adafruit_PCA9685.PCA9685()
 pwm.set_pwm_freq(60)
-m = MissileDefense(375, 80, pwm, left_pro, TURRET_1, TURRET_2, TURRET_3)
+m = MissileDefense(375, 80, pwm, left_pro, TURRET_2, TURRET_3, TURRET_4)
 m.play_on()
