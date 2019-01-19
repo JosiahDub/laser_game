007ca0207f544cb5cc44188e1d9b03f1670d47d7
Josiah White <josiahwhite@gmail.com>
2019-01-19 13:05:29 -0500

    Moving turret to center.

--- play_maze.py
+++ play_maze.py
@@ -1,6 +1,6 @@
 import Adafruit_PCA9685
 from games import Maze
-from src.TURRETS import TURRET_1
+from src.TURRETS import TURRET_3
 from src import pro_controller_factory
 
 left, right = pro_controller_factory()
@@ -8,5 +8,5 @@ left, right = pro_controller_factory()
 pwm = Adafruit_PCA9685.PCA9685()
 pwm.set_pwm_freq(60)
 
-maze = Maze(375, 100, pwm, left, TURRET_1)
+maze = Maze(375, 100, pwm, left, TURRET_3)
 maze.play_on()
