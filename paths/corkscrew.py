a9a82e31b880274d0ec7d5500f9708e8e3a25504
Josiah White <josiahwhite@gmail.com>
2019-01-20 16:01:02 -0500

    Fixing problem of infinite corkscrew.

--- paths/corkscrew.py
+++ paths/corkscrew.py
@@ -21,10 +21,10 @@ class Corkscrew(Circle):
         angle_rate = self._angle + self._rate
         while keep_going:
             keep_going = False
-            if fabs(x - self.x_end) > fabs(self.x_rate):
+            if fabs(self.x_center - self.x_end) > fabs(self.x_rate):
                 self.x_center += self.x_rate
                 keep_going = True
-            if fabs(y - self.y_end) > fabs(self.y_rate):
+            if fabs(self.y_center - self.y_end) > fabs(self.y_rate):
                 self.y_center += self.y_rate
                 keep_going = True
             x = self._radius * sin(angle_rate) + self.x_center + self.x_rate
