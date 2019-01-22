456a05bf81794ed2da81fc0ed0c404b0cab378cb
Josiah White <josiahwhite@gmail.com>
2019-01-21 23:03:05 -0500

    Finally moving the turrets to the center.
    Removing commented code.

--- play_pong.py
+++ play_pong.py
@@ -1,11 +1,10 @@
 import Adafruit_PCA9685
 from games import Pong
-from src.TURRETS import TURRET_1, TURRET_2, TURRET_3
+from src.TURRETS import TURRET_2, TURRET_3, TURRET_4
 from src import pro_controller_factory
 
-# nunchuk_1 = PlayerNunchuk(127, 127, 20, 200, 20, 200)
 c_1, c_2 = pro_controller_factory()
 pwm = Adafruit_PCA9685.PCA9685()
 pwm.set_pwm_freq(60)
-p = Pong(375, 100, pwm, c_1, c_2, TURRET_1, TURRET_3, TURRET_2)
+p = Pong(375, 100, pwm, c_1, c_2, TURRET_2, TURRET_4, TURRET_3)
 p.play_on()
