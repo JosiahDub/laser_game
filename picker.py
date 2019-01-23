c566ae3fab7fb220205ec45a8495c5399c7ce87e
Josiah White <josiahwhite@gmail.com>
2019-01-22 21:16:48 -0500

    Adding this instead of downloading the package manually.

--- picker.py
+++ picker.py
@@ -24,13 +24,27 @@ class Picker:
         mainloop()
 
     def play_maze(self):
-        pass
+        left, right = pro_controller_factory()
+
+        pwm = Adafruit_PCA9685.PCA9685()
+        pwm.set_pwm_freq(60)
+
+        maze = Maze(375, 100, pwm, left, TURRET_3)
+        maze.play_on()
 
     def play_pong(self):
-        pass
+        c_1, c_2 = pro_controller_factory()
+        pwm = Adafruit_PCA9685.PCA9685()
+        pwm.set_pwm_freq(60)
+        p = Pong(375, 100, pwm, c_1, c_2, TURRET_2, TURRET_4, TURRET_3)
+        p.play_on()
 
     def play_missile_defense(self):
-        pass
+        left_pro, _ = pro_controller_factory()
+        pwm = Adafruit_PCA9685.PCA9685()
+        pwm.set_pwm_freq(60)
+        m = MissileDefense(375, 80, pwm, left_pro, TURRET_2, TURRET_3, TURRET_4)
+        m.play_on()
 
 
 if __name__ == '__main__':
