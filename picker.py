661d9b0751a17a48615a1240ebf457225fa2ee96
Josiah White <josiahwhite@gmail.com>
2019-01-22 21:54:37 -0500

    Prettying it up with le epiq dark mode.
    Got rid of event by binding to lambdas, which also fixes clicking the buttons.

--- picker.py
+++ picker.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import font
 import Adafruit_PCA9685
 from games import Pong, MissileDefense, Maze
 from src.TURRETS import TURRET_2, TURRET_3, TURRET_4
@@ -9,6 +10,7 @@ class Picker:
 
     def __init__(self):
         self.root = Tk()
+        f = font.Font(family='Helvetica', size=90, weight=font.BOLD)
         self.root.columnconfigure(0, weight=1)
         self.root.rowconfigure(0, weight=1)
         self.root.rowconfigure(1, weight=1)
@@ -16,20 +18,25 @@ class Picker:
         self.root.geometry('1920x1080')
 
         self.maze_button = Button(self.root, text='Maze',
-                                  command=self.play_maze)
-        self.maze_button.bind('<Return>', self.play_maze)
+                                  command=self.play_maze, font=f,
+                                  bg='#303030', fg='white')
+        self.maze_button.bind('<Return>', lambda event:self.play_maze())
         self.maze_button.grid(row=0, column=0, sticky=N+S+E+W)
         self.pong_button = Button(self.root, text='Pong',
-                                  command=self.play_pong)
-        self.pong_button.bind('<Return>', self.play_pong)
+                                  command=self.play_pong, font=f,
+                                  bg='#303030', fg='white')
+        self.pong_button.bind('<Return>',
+                              lambda event:self.play_pong())
         self.pong_button.grid(row=1, column=0, sticky=N+S+E+W)
         self.missile_defense_button = Button(self.root, text='Missile Defense',
-                                             command=self.play_missile_defense)
-        self.missile_defense_button.bind('<Return>', self.play_missile_defense)
+                                             command=self.play_missile_defense,
+                                             font=f, bg='#303030', fg='white')
+        self.missile_defense_button.bind('<Return>',
+                                         lambda event:self.play_missile_defense())
         self.missile_defense_button.grid(row=2, column=0, sticky=N+S+E+W)
         mainloop()
 
-    def play_maze(self, _):
+    def play_maze(self):
         left, right = pro_controller_factory()
 
         pwm = Adafruit_PCA9685.PCA9685()
@@ -38,14 +45,14 @@ class Picker:
         maze = Maze(375, 100, pwm, left, TURRET_3)
         maze.play_on()
 
-    def play_pong(self, _):
+    def play_pong(self):
         c_1, c_2 = pro_controller_factory()
         pwm = Adafruit_PCA9685.PCA9685()
         pwm.set_pwm_freq(60)
         p = Pong(375, 100, pwm, c_1, c_2, TURRET_2, TURRET_4, TURRET_3)
         p.play_on()
 
-    def play_missile_defense(self, _):
+    def play_missile_defense(self):
         left_pro, _ = pro_controller_factory()
         pwm = Adafruit_PCA9685.PCA9685()
         pwm.set_pwm_freq(60)
